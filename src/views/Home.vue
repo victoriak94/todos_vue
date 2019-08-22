9e1884cba076e64f10078318418303705b149ef6
victoriak94 <vkamila6@gmail.com>
2019-08-22 13:10:32 -0400

    Install axios. Make http request to jsonplaceholder for todos. Create deletetodos method

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
 
 <script>
 import Todos from "../components/Todos";
+import axios from "axios";
 
 export default {
   name: "home",
@@ -14,6 +15,22 @@ export default {
     return {
       todos: []
     };
+  },
+  methods: {
+    deleteTodo(id) {
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => (this.todos = (this.todos.filter(todo => todo.id !== id)))
+      .catch(err => console.log(err));
+    },
+    addTodo() {
+      
+    }
+  },
+  created() {
+    axios
+    .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+    .then(res => (this.todos = res.data))
+    .catch(err => console.log(err));
   }
 };
 </script>
