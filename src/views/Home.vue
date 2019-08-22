66b074dfbda767ca7cdd3b40b89afa4b63c1e38b
victoriak94 <vkamila6@gmail.com>
2019-08-22 13:32:02 -0400

    Create addTodos method

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,19 +18,25 @@ export default {
   },
   methods: {
     deleteTodo(id) {
-      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-      .then(res => (this.todos = (this.todos.filter(todo => todo.id !== id)))
-      .catch(err => console.log(err));
+      axios
+        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(res => (this.todos = this.todos.filter(todo => todo.id !== id)))
+        .catch(err => console.log(err));
     },
-    addTodo() {
-      
+    addTodo(newTodo) {
+      const { title, compeleted } = newTodo;
+
+      axios
+        .post("https://jsonplaceholder.typicode.com/todos")
+        .then(res => (this.todos = [...this.todos, res.data]));
+        .catch(err => console.log(err));
     }
   },
   created() {
     axios
-    .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
-    .then(res => (this.todos = res.data))
-    .catch(err => console.log(err));
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then(res => (this.todos = res.data))
+      .catch(err => console.log(err));
   }
 };
 </script>
