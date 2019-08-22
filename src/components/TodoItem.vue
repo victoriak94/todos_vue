58156bce06d0c47aa9f56d95b5971ea8b20b4cb9
victoriak94 <vkamila6@gmail.com>
2019-08-22 14:53:07 -0400

    Bind is-complete class to TodoItem. Add checkbox input and create markComplete method

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,13 +1,21 @@
 <template>
-  <div class="todo-item">
-    <p>{{todo.title}}</p>
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+    <p>
+      <input type="checkbox" v-on:change="markComplete" />
+      {{todo.title}}
+    </p>
   </div>
 </template>
 
 <script>
 export default {
   name: "TodoItem",
-  props: ["todo"]
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    }
+  }
 };
 </script>
 
