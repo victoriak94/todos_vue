16ebcde9d058e025ccb4b22f0ffb5a0982d6bd5e
victoriak94 <vkamila6@gmail.com>
2019-08-22 23:46:40 -0400

    Refactor methods in Home.vue. Refactor About.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,10 +8,9 @@
 <script>
 import Todos from "../components/Todos";
 import AddTodo from "../components/AddTodo";
-import axios from "axios";
 
 export default {
-  name: "home",
+  name: "Home",
   components: {
     Todos,
     AddTodo
@@ -29,17 +28,19 @@ export default {
         .catch(err => console.log(err));
     },
     addTodo(newTodo) {
-      const { title, compeleted } = newTodo;
-
+      const { title, completed } = newTodo;
       axios
-        .post("https://jsonplaceholder.typicode.com/todos")
+        .post("https://jsonplaceholder.typicode.com/todos", {
+          title,
+          completed
+        })
         .then(res => (this.todos = [...this.todos, res.data]))
         .catch(err => console.log(err));
     }
   },
   created() {
     axios
-      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=5")
       .then(res => (this.todos = res.data))
       .catch(err => console.log(err));
   }
@@ -47,5 +48,25 @@ export default {
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
+}
 </style>
 
